18cdcb9b11565768d2bde6ffd9160a063ec122c0
TheIrishBub <55152442+TheIrishBub@users.noreply.github.com>
2020-02-06 14:40:55 -0500

    Fixed some formatting errors

--- maybe.py
+++ maybe.py
@@ -11,6 +11,8 @@ print(dedent("""\nYou find the cave near the village and enter.
 After walking along, you find a treasure chest guarded by a bat.
 You engage the bat!"""))
 
+# Bat fight
+
 bat_hp = 10
 bat_attack = 5
 bat_defense = 0
@@ -46,20 +48,22 @@ while player_hp > 0 and bat_hp > 0:
             damage = 0
         player_hp = player_hp - damage
         print(f"The bat hit you for {damage} damage! You have {player_hp} health left.")
+
     elif choice == '3':
         healing = 20 + random.randint(0,5)
         player_hp = player_hp + healing
         if player_hp > 50:
             player_hp = 50
         print(f"\nYou restored {healing} health. You have {player_hp} health.")
-    elif choice == '69':
-        print("Nice.")
+
     else:
         print("Try again.")
 
 print(dedent("""\nYou continue forward, and as you go, you start to feel a chill
 in the air. You are wondering why you are feeling so cold when suddenly,
 a menacing ghost comes out of nowhere and attacks you!"""))
+
+# Ghost fight
 
 ghost_hp = 40
 ghost_attack = 10
@@ -96,14 +100,14 @@ while player_hp > 0 and ghost_hp > 0:
             damage = 0
         player_hp = player_hp - damage
         print(f"The ghost hit you for {damage} damage! You have {player_hp} health left.")
+
     elif choice == '3':
         healing = 20 + random.randint(0,5)
         player_hp = player_hp + healing
         if player_hp > 50:
             player_hp = 50
         print(f"\nYou restored {healing} health. You have {player_hp} health.")
-    elif choice == '69':
-        print("Nice.")
+
     else:
         print("Try again.")
 
@@ -112,6 +116,8 @@ resembles a graveyard. As you explore this new area, you examine all the
 graves. When looking at the back row, you hear a shuffling sound coming
 from the side. You turn to look, and see a skeleton rising from its grave.
 The skeleton, wanting to take you with him, attacks you!"""))
+
+# Skeleton fight
 
 skeleton_hp = 50
 skeleton_attack = 15
@@ -148,20 +154,22 @@ while player_hp > 0 and skeleton_hp > 0:
             damage = 0
         player_hp = player_hp - damage
         print(f"The skeleton hit you for {damage} damage! You have {player_hp} health left.")
+
     elif choice == '3':
         healing = 20 + random.randint(0,5)
         player_hp = player_hp + healing
         if player_hp > 50:
             player_hp = 50
         print(f"\nYou restored {healing} health. You have {player_hp} health.")
-    elif choice == '69':
-        print("Nice.")
+
     else:
         print("Try again.")
 
-print(dedent("""\nAfter shaking off the skeleton, you decide to leave the graveyard area of this cave.
-You take a path leading away from the graveyard,
+print(dedent("""\nAfter shaking off the skeleton, you decide to leave the
+graveyard area of this cave. You take a path leading away from the graveyard,
 when a goblin jumps out and ambushes you!"""))
+
+# Goblin fight
 
 goblin_hp = 100
 goblin_attack = 10
@@ -198,25 +206,26 @@ while player_hp > 0 and goblin_hp > 0:
             damage = 0
         player_hp = player_hp - damage
         print(f"The goblin hit you for {damage} damage! You have {player_hp} health left.")
+
     elif choice == '3':
         healing = 20 + random.randint(0,5)
         player_hp = player_hp + healing
         if player_hp > 50:
             player_hp = 50
-        print(f"\nYou restored {healing} health. You have {player_hp} health.")
-    elif choice == '69':
-        print("Nice.")
+
     else:
         print("Try again.")
 
-print(dedent("""\nWith the goblin slain, you continue on. As you find your way to the back of the
-cave, you hear a roaring sound. You turn the corner to discover the fearsome
-dragon that has been terrorizing the nearby village!
+print(dedent("""\nWith the goblin slain, you continue on. As you find your way
+to the back of the cave, you hear a roaring sound. You turn the corner to
+discover the fearsome dragon that has been terrorizing the nearby village!
 In order to restore peace, you start fighting the dragon."""))
+
+# Dragon fight
 
 dragon_hp = 150
 dragon_attack = 20
-dragon_defense = 2
+dragon_defense = 5
 player_hp = 50
 player_attack = 25
 player_defense = 15
@@ -248,16 +257,17 @@ while player_hp > 0 and dragon_hp > 0:
             damage = 0
         player_hp = player_hp - damage
         print(f"The dragon hit you for {damage} damage! You have {player_hp} health left.")
+
     elif choice == '3':
         healing = 20 + random.randint(0,5)
         player_hp = player_hp + healing
         if player_hp > 50:
             player_hp = 50
         print(f"\nYou restored {healing} health. You have {player_hp} health.")
-    elif choice == '69':
-        print("Nice.")
+
     else:
         print("Try again.")
+
 
 if player_hp <= 0:
     print("You died!")
